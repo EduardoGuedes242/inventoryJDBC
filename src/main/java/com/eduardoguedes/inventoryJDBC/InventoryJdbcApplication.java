6ae582412e96bef1353dc84c8c922091c6372728
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-08-04 08:45:31 -0300

    images

--- src/main/java/com/eduardoguedes/inventoryJDBC/InventoryJdbcApplication.java
+++ src/main/java/com/eduardoguedes/inventoryJDBC/InventoryJdbcApplication.java
@@ -2,6 +2,7 @@ package com.eduardoguedes.inventoryJDBC;
 
 import com.eduardoguedes.inventoryJDBC.product.entity.Product;
 import com.eduardoguedes.inventoryJDBC.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
